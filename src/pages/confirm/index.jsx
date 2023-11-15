4e85e74cb5267bf8fd5e86f0b0accc0eca87b8e2
Амарсанаа Төмөрбаатар <LP230107@G301-CC0165.local>
2023-11-15 18:37:30 +0800

    shine

---
+++ src/pages/confirm/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Confirm() {
+  return (
+    <div className="">
+      <div className="flex justify-center mb-8">
+        <div>
+          <img src="./geldlogo.svg"></img>
+        </div>
+        <div>
+          <img src="./geld.svg"></img>
+        </div>
+
+        <div className="flex-col items-center flex">
+          <div className="w-96 h-96">
+            <img src="./confirm.svg"></img>
+          </div>
+          <h1 className="font-bold text-xl">Select base currency</h1>
+          <input className="border-2 rounded-xl h-10 bg-slate-200 w-full mb-4 mt-6"></input>
+          <p className="w-80 mb-6">
+            Your base currency should be the one you use most often. All
+            transaction in other currencies will be calculated based on this one
+          </p>
+          <button className="bg-blue-600 text-white rounded-xl w-full h-10">
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
